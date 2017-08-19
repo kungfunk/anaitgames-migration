52faa050bd136e198ac56a79e71d7125b101d328
kungfunk <kung.funk@gmail.com>
2017-08-18 17:14:25 -0700

    fixed cache

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-error_reporting(E_ERROR | E_WARNING | E_PARSE);
-
-const HOST = 'localhost';
-const DB_OLD = 'anaitgames_old';
-const DB_NEW = 'anaitgames_new';
-const USER = 'user';
-const PASS = 'pass';
