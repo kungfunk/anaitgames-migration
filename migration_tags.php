8bc6f628e99c3173d2cfeb2267d8e85f5a8bde2c
Víctor Calzón Fraile <victor.calzonfraile@kbc.be>
2017-08-30 12:23:25 +0200

    Added migration from games, platforms and cards to new tag system.

--- migration_tags.php
+++ migration_tags.php
@@ -4,39 +4,110 @@
 include config.php
 
 const SQL_SELECT_PLATAFORMA = "SELECT * FROM plataforma LIMIT 50";
-const SQL_INSERT_FIXED_PLATAFORMA = "INSERT INTO tags (name, slug) VALUES (:name, :slug)";
+const SQL_SELECT_FICHA = "SELECT * FROM fichas LIMIT 50";
+const SQL_SELECT_JUEGO = "SELECT * FROM juego LIMIT 50"
+const SQL_INSERT_FIXED_TAG = "INSERT INTO tags (name, slug) VALUES (:name, :slug)";
 const SQL_SELECT_PLATAFORMA_RELATIONSHIP = "SELECT * FROM articulo_plataforma WHERE id_plataforma = :id_plataforma";
+const SQL_SELECT_FICHA_RELATIONSHIP = "SELECT * FROM articulo_fichas_relacionados WHERE id_ficha = :id_ficha";
+const SQL_SELECT_JUEGO_RELATIONSHIP = "SELECT * FROM articulo_juegos_relacionados WHERE id_juego = :id_juego"
 const SQL_SELECT_NEW_ARTICULO_ID = "SELECT id_new FROM _temp_post_id WHERE id_old = :id_old";
-const SQL_INSERT_FIXED_PLATAFORMA_RELATIONSHIP = "INSERT INTO posts_tags (post_id, tag_id) VALUES (:post_id, :tag_id)";
+const SQL_INSERT_FIXED_TAG_RELATIONSHIP = "INSERT INTO posts_tags (post_id, tag_id) VALUES (:post_id, :tag_id)";
 
 print "-----STARTING TAGS (platform) MIGRATION -----" . PHP_EOL;
 
 foreach($db_old->query(SQL_SELECT_PLATAFORMA) as $platform) {
-    $insert_fixed_platform = $db_new->prepare(SQL_INSERT_FIXED_PLATAFORMA);
-    $insert_fixed_platform->execute([
+    $insert_fixed_tag = $db_new->prepare(SQL_INSERT_FIXED_TAG);
+    $insert_fixed_tag->execute([
         ':name' => $platform->nombre,
         ':slug' => $platform->url
     ]);
 
-    $platform_new_id = $db_new->lastInsertId();
+    $tag_id = $db_new->lastInsertId();
 
-    $platform_relationship = $db_old->prepare(SQL_SELECT_PLATAFORMA_RELATIONSHIP);
-    $platform_relationship->execute([
+    $select_relationships = $db_old->prepare(SQL_SELECT_PLATAFORMA_RELATIONSHIP);
+    $select_relationships->execute([
         ':id_plataforma' => $platform->id
     ]);
 
-    foreach($platform_relationship->fetch(PDO::FETCH_OBJ as $relationship) {
+    foreach($select_relationships->fetch(PDO::FETCH_OBJ as $relationship) {
         $select_post_new_id = $db_new->prepare(SQL_SELECT_NEW_ARTICULO_ID);
         $select_post_new_id->execute([
-            ':'
+            ':id_old' => $relationship->id_articulo
         ]);
+        $post_new_id = $select_post_new_id->fetch(PDO::FETCH_OBJ)->id_new;
 
-        $insert_platform_fixed_relationship = $db_new->prepare(SQL_INSERT_FIXED_PLATAFORMA_RELATIONSHIP);
-        $insert_platform_fixed_relationship->execute([
-            ':tag_id' => $platform_new_id,
+        $insert_tag_relationship = $db_new->prepare(SQL_INSERT_FIXED_TAG_RELATIONSHIP);
+        $insert_tag_relationship->execute([
+            ':tag_id' => $tag_id,
             ':post_id' => $post_new_id
         ]);
     }
 }
 
 print "-----TAGS (plaform) MIGRATION ENDED-----" . PHP_EOL;
+
+print "-----STARTING TAGS (ficha) MIGRATION -----" . PHP_EOL;
+
+foreach($db_old->query(SQL_SELECT_FICHA) as $ficha) {
+    $insert_fixed_tag = $db_new->prepare(SQL_INSERT_FIXED_TAG);
+    $insert_fixed_tag->execute([
+        ':name' => $ficha->nombre,
+        ':slug' => $ficha->url
+    ]);
+
+    $tag_id = $db_new->lastInsertId();
+
+    $select_relationships = $db_old->prepare(SQL_SELECT_FICHA_RELATIONSHIP);
+    $select_relationships->execute([
+        ':id_ficha' => $ficha->id
+    ]);
+
+    foreach($select_relationships->fetch(PDO::FETCH_OBJ as $relationship) {
+        $select_post_new_id = $db_new->prepare(SQL_SELECT_NEW_ARTICULO_ID);
+        $select_post_new_id->execute([
+            ':id_old' => $relationship->id_articulo
+        ]);
+        $post_new_id = $select_post_new_id->fetch(PDO::FETCH_OBJ)->id_new;
+
+        $insert_tag_relationship = $db_new->prepare(SQL_INSERT_FIXED_TAG_RELATIONSHIP);
+        $insert_tag_relationship->execute([
+            ':tag_id' => $tag_id,
+            ':post_id' => $post_new_id
+        ]);
+    }
+}
+
+print "-----TAGS (ficha) MIGRATION ENDED-----" . PHP_EOL;
+
+print "-----STARTING TAGS (juegos) MIGRATION -----" . PHP_EOL;
+
+foreach($db_old->query(SQL_SELECT_JUEGO) as $juego) {
+    $insert_fixed_tag = $db_new->prepare(SQL_INSERT_FIXED_TAG);
+    $insert_fixed_tag->execute([
+        ':name' => $juego->nombre,
+        ':slug' => $juego->url
+    ]);
+
+    $tag_id = $db_new->lastInsertId();
+
+    $select_relationships = $db_old->prepare(SQL_SELECT_JUEGO_RELATIONSHIP);
+    $select_relationships->execute([
+        ':id_juego' => $juego->id
+    ]);
+
+    foreach($select_relationships->fetch(PDO::FETCH_OBJ as $relationship) {
+        $select_post_new_id = $db_new->prepare(SQL_SELECT_NEW_ARTICULO_ID);
+        $select_post_new_id->execute([
+            ':id_old' => $relationship->id_articulo
+        ]);
+        $post_new_id = $select_post_new_id->fetch(PDO::FETCH_OBJ)->id_new;
+
+        $insert_tag_relationship = $db_new->prepare(SQL_INSERT_FIXED_TAG_RELATIONSHIP);
+        $insert_tag_relationship->execute([
+            ':tag_id' => $tag_id,
+            ':post_id' => $post_new_id
+        ]);
+    }
+}
+
+print "-----TAGS (juegos) MIGRATION ENDED-----" . PHP_EOL;
